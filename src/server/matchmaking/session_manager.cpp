39ad799595bfaa0b9f25f55d9214c701c27336fa
Mikhail Kashin <dobordx@yandex.ru>
2025-08-25 16:43:41 +0300

    chore(log): downgrade input change logs to debug level

--- src/server/matchmaking/session_manager.cpp
+++ src/server/matchmaking/session_manager.cpp
@@ -105,7 +105,7 @@ void SessionManager::update_input(const std::shared_ptr<Session> &s, const t2d::
     s->input.fire = cmd.fire();
     s->input.brake = cmd.brake();
     if (!s->is_bot && (move_changed || turn_changed || turret_changed || fire_changed || brake_changed)) {
-        t2d::log::info(
+        t2d::log::debug(
             "[input] session={} ctick={} move={} turn={} turret={} fire={} brake={}",
             s->session_id,
             s->input.last_client_tick,
